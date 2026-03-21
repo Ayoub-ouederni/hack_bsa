a30b4a3e73f2cb362a2ebb3e72b4f74c5390def8
Ayoub-ouederni <ayoub.ouederni@epfl.ch>
2026-03-22 00:11:46 +0100

    feat: add SignerEntryInfo and SignerListInfo types

--- src/lib/xrpl/account.ts
+++ src/lib/xrpl/account.ts
@@ -2,6 +2,16 @@ import type { AccountInfoResponse, AccountObjectsResponse } from "xrpl";
 import { xrpToDrops } from "../utils/xrp";
 import { getClient } from "./client";
 
+export interface SignerEntryInfo {
+  account: string;
+  signerWeight: number;
+}
+
+export interface SignerListInfo {
+  signerQuorum: number;
+  signerEntries: SignerEntryInfo[];
+}
+
 export async function getBalance(address: string): Promise<number> {
   const client = await getClient();
   const xrpBalance = await client.getXrpBalance(address);
