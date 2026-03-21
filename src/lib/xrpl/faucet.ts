e37c66a50282b2d684278bd96ac04bf08ac311b8
Ayoub-ouederni <ayoub.ouederni@epfl.ch>
2026-03-22 00:14:30 +0100

    feat: add createTestnetWallet using XRPL faucet

--- src/lib/xrpl/faucet.ts
+++ src/lib/xrpl/faucet.ts
@@ -5,3 +5,9 @@ export interface FaucetResult {
   wallet: Wallet;
   balance: number;
 }
+
+export async function createTestnetWallet(): Promise<FaucetResult> {
+  const client = await getClient();
+  const { wallet, balance } = await client.fundWallet();
+  return { wallet, balance };
+}
