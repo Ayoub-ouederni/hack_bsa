b14fb8da3fadd0d4a6d8b81557dfecb89ccef0bd
Ayoub-ouederni <ayoub.ouederni@epfl.ch>
2026-03-22 00:10:55 +0100

    feat: return balance in drops to match project conventions

--- src/lib/xrpl/account.ts
+++ src/lib/xrpl/account.ts
@@ -1,7 +1,8 @@
+import { xrpToDrops } from "../utils/xrp";
 import { getClient } from "./client";
 
 export async function getBalance(address: string): Promise<number> {
   const client = await getClient();
-  const balance = await client.getXrpBalance(address);
-  return balance;
+  const xrpBalance = await client.getXrpBalance(address);
+  return xrpToDrops(xrpBalance);
 }
