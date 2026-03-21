75ebf46f81fba1132304d6a6d000d1e737aabe2d
Ayoub-ouederni <ayoub.ouederni@epfl.ch>
2026-03-22 00:20:40 +0100

    feat: add barrel exports for account module functions and types

--- src/lib/xrpl/index.ts
+++ src/lib/xrpl/index.ts
@@ -6,3 +6,28 @@ export {
   getNetworkId,
   withClient,
 } from "./client";
+
+export {
+  getBalance,
+  getAccountInfo,
+  getTxHistory,
+  getSignerList,
+  getAvailableBalance,
+  getEscrows,
+  getNFTs,
+  accountExists,
+  getAccountSequence,
+  getLedgerIndex,
+  getServerFee,
+  getOwnerCount,
+  calculateReserve,
+  isValidAddress,
+} from "./account";
+
+export type {
+  SignerEntryInfo,
+  SignerListInfo,
+  TxHistoryEntry,
+  EscrowInfo,
+  NFTokenInfo,
+} from "./account";
