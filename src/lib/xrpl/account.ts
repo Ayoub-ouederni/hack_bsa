f5abf21cfb95ed64c86f2b503c374cc2671583b3
Ayoub-ouederni <ayoub.ouederni@epfl.ch>
2026-03-22 00:10:41 +0100

    feat: add getBalance function returning XRP balance as number

--- src/lib/xrpl/account.ts
+++ src/lib/xrpl/account.ts
@@ -1,1 +1,7 @@
 import { getClient } from "./client";
+
+export async function getBalance(address: string): Promise<number> {
+  const client = await getClient();
+  const balance = await client.getXrpBalance(address);
+  return balance;
+}
